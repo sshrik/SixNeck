b0ab4ae1ec0b371a8cc398bd98d64451a97b3e0d
최현준 <chj878194@naver.com>
2017-05-14 14:15:52 +0900

    Make draw and error correction
    Make draw and error correction

--- SixNeck Project/sixneck.c
+++ SixNeck Project/sixneck.c
@@ -3,8 +3,8 @@
 #include<time.h>
 /*
 [목표] : 6칸을 만들어 승리 / 상대의 6칸을 방해
-1. 탐색하여 my_state 와 enemy_state 를 채운다. 
-	-> void search_state(int ms[][MAP_LENGTH], int es[][MAP_LENGTH], int priority[], int mine);
+1. 탐색하여 my_state 와 enemy_state 를 채운다.
+-> void search_state(int ms[][MAP_LENGTH], int es[][MAP_LENGTH], int priority[], int mine);
 맵을 정의하여야 한다.
 */
 #define MAP_LENGTH 20
@@ -20,27 +20,27 @@
 #define BLOCKING 3
 
 /*
-2. 행동 조건에 따라 행동한다. 
-	-> void act(int ms[][MAP_LENGTH], int es[][MAP_LENGTH], int priority[], int mine);
-- 내꺼가 이기는 상태( mw, aw ) 가 1개라도 있으면 바로 승리하게 둔다.	
-	-> int mw_location( int ms[][MAP_LENGTH], location where[], int priority[], int mine );
-	-> int aw_location( int ms[][MAP_LENGTH], location where[], int priority[], int mine );
-	-> int can_win(int ms[][MAP_LENGTH], int priority[], int mine);
-- 상대꺼가 이기는 상태 ( mw, aw ) 가 1개라도 있으면 바로 막게 된다.		
-	-> int can_lose(int es[][MAP_LENGTH], int priority[], int mine);
+2. 행동 조건에 따라 행동한다.
+-> void act(int ms[][MAP_LENGTH], int es[][MAP_LENGTH], int priority[], int mine);
+- 내꺼가 이기는 상태( mw, aw ) 가 1개라도 있으면 바로 승리하게 둔다.
+-> int mw_location( int ms[][MAP_LENGTH], location where[], int priority[], int mine );
+-> int aw_location( int ms[][MAP_LENGTH], location where[], int priority[], int mine );
+-> int can_win(int ms[][MAP_LENGTH], int priority[], int mine);
+- 상대꺼가 이기는 상태 ( mw, aw ) 가 1개라도 있으면 바로 막게 된다.
+-> int can_lose(int es[][MAP_LENGTH], int priority[], int mine);
 - 위의 두 조건이 아닐 시, 후보 위치군을 추출하여 내 가중치를 최대화, 상대 가중치를 최소화시키는
-	방향으로 행동한다.	
-	-> void find_candidate_location(int ms[][MAP_LENGTH],int es[][MAP_LENGTH],location candidate[], int priority[], int mine)
+방향으로 행동한다.
+-> void find_candidate_location(int ms[][MAP_LENGTH],int es[][MAP_LENGTH],location candidate[], int priority[], int mine)
 - 가중치는 int priority[state_length] 로 정의 되어 있다.
 3. 둔다.
-	->	location decide_location( int ms[][MAP_LENGTH], int es[][MAP_LENGTH], int priority[], int mine )
-	-> void add_stone(int who, location where, int map[][MAP_LENGTH], int mine)
+->	location decide_location( int ms[][MAP_LENGTH], int es[][MAP_LENGTH], int priority[], int mine )
+-> void add_stone(int who, location where, int map[][MAP_LENGTH], int mine)
 4. 승리 조건 판별, 다음 차례가 누구인가 판별 후 진행.
-	-> int who_win( int map[][MAP_LENGTH] );
+-> int who_win( int map[][MAP_LENGTH] );
 5. 승리 / 패배 시 종료. 다음 차례는 1로 가서 계속 진행.
-	-> void initialize_map( int map[][MAP_LENGTH], int mine );
-	-> void array1_initailizer( int arr[], int length, int toInitialize );
-	-> void array2_initailizer( int arr[][MAP_LENGTH], int toInitialize );
+-> void initialize_map( int map[][MAP_LENGTH], int mine );
+-> void array1_initailizer( int arr[], int length, int toInitialize );
+-> void array2_initailizer( int arr[][MAP_LENGTH], int toInitialize );
 
 이를 위해서 상태를 정의하고, 가중치를 부여해야 한다.
 #deifne NONE_STATE -1
@@ -51,8 +51,8 @@ NONE_STATE 는 -1,
 _0 은 0, _1 은 1 .. _n 은 n 을 각각에 대해서 더하는 것으로 진행한다.
 즉 ALPHA3_6 = 4 * 9 + 6 이다.
 ... 으로 상태를 정의하도록 한다.
-또한 mw, aw 의 상태는 각 숫자를 써서 프로그래머가 기억하고, 각각 분류 해 준다. 
-	- 4, 5 개는 전부 aw 이상이니 분류가 쉬울 것이다.
+또한 mw, aw 의 상태는 각 숫자를 써서 프로그래머가 기억하고, 각각 분류 해 준다.
+- 4, 5 개는 전부 aw 이상이니 분류가 쉬울 것이다.
 또한 방향은 */
 #define NORTH_EAST 0
 #define EAST 1
@@ -86,7 +86,7 @@ int priority[state_length];
 #define _7 7
 #define _8 8
 /*
-	State is defigned like DELTA5 * _5 or GAMMA7 * _2
+State is defigned like DELTA5 * _5 or GAMMA7 * _2
 */
 #define NONE_STATE - 1
 /*
@@ -96,7 +96,7 @@ int priority[state_length];
 #define EPSILONE 0
 /*
 α	1개의 내 돌 ( ○ ) 존재 = { α0, α1, α2 ... α5 }
-α0	○◎◎◎◎◎	α1	◎○◎◎◎◎	α2	◎◎○◎◎◎	α3	◎◎◎○◎◎	α4	◎◎◎◎○◎	
+α0	○◎◎◎◎◎	α1	◎○◎◎◎◎	α2	◎◎○◎◎◎	α3	◎◎◎○◎◎	α4	◎◎◎◎○◎
 α5	◎◎◎◎◎○
 */
 #define ALPHA0 1
@@ -215,6 +215,7 @@ aw set : { δ, θ set } X { _0, _2, _6, _8 }
 #define PARENT_MAX 4
 #define CROSS_POINT_MAX 30
 #define SIVILING_MAX 5
+#define FITNESS_MAX 5000
 
 typedef struct {
 	short x;
@@ -239,8 +240,11 @@ unsigned long long int get_state_priority(int state[][MAP_LENGTH][MAP_LENGTH], i
 int who_win(int map[][MAP_LENGTH], location where_put);
 int dir_row_win(int map[][MAP_LENGTH], location where_put, location dir, int mine);
 int dir_win(int map[][MAP_LENGTH], location where_put, location dir, int mine);
+int is_draw(int map[][MAP_LENGTH]);
 location find_candidate_location(int map[][MAP_LENGTH], int ms[][MAP_LENGTH][MAP_LENGTH], int es[][MAP_LENGTH][MAP_LENGTH], int priority[], int mine);
 int add_stone(location where, int map[][MAP_LENGTH], int mine);
+void ai_turn(int map[][MAP_LENGTH], int priority[], location last_put);
+int ai_game(int map[][MAP_LENGTH], int p0[], int p1[]);
 
 // Utilization Functions
 void initialize_map(int map[][MAP_LENGTH]);
@@ -256,14 +260,12 @@ void select_parent(int parent[][STATE_LENGTH], int selected[][STATE_LENGTH], int
 void set_fitness(int parent[][STATE_LENGTH], int fitness[]);
 void generate_generation(int geneartion[][STATE_LENGTH], int parent[][STATE_LENGTH]);
 void cross_product(int p0[], int p1[], int gen[], int cross[]);
+void generate_cross_point(int cross[]);
 void generate_mutation(int generation[][STATE_LENGTH], int fitness[]);
 void suffle_generation(int generation[][STATE_LENGTH]);
 int real_rand(int from, int to);
 
 int main() {
-	int priority[STATE_LENGTH];
-	int map[MAP_LENGTH][MAP_LENGTH];
-
 	srand((unsigned)time(NULL));
 
 
@@ -272,8 +274,8 @@ int main() {
 
 void search_state(int ms[][MAP_LENGTH][MAP_LENGTH], int es[][MAP_LENGTH][MAP_LENGTH], int map[][MAP_LENGTH], int mine) {
 	/*	Check my state(ms), enemy sate(es) from map and save to there.
-		'mine' will be BLACK or WHITE, which is my color.
-		State will save like ALPHA0 * _0 */
+	'mine' will be BLACK or WHITE, which is my color.
+	State will save like ALPHA0 * _0 */
 	int i, j, d;
 	vector start;
 
@@ -924,7 +926,7 @@ int mw_location(int state[][MAP_LENGTH][MAP_LENGTH], vector where[], location di
 
 	for (k = 0; k < DIR_MAX; k++) {
 		temp.dir = k;	// Set dir to k.
-						// We already defined NORTH_EAST to 0 ... etc.
+		// We already defined NORTH_EAST to 0 ... etc.
 		for (i = 0; i < MAP_LENGTH; i++) {
 			temp.x = i;	// Set x to i.
 			for (j = 0; j < MAP_LENGTH; j++) {
@@ -998,7 +1000,7 @@ int aw_location(int state[][MAP_LENGTH][MAP_LENGTH], vector where[]) {
 
 	for (k = 0; k < DIR_MAX; k++) {
 		temp.dir = k;	// Set dir to k.
-						// We already defined NORTH_EAST to 0 ... etc.
+		// We already defined NORTH_EAST to 0 ... etc.
 		for (i = 0; i < MAP_LENGTH; i++) {
 			temp.x = i;	// Set x to i.
 			for (j = 0; j < MAP_LENGTH; j++) {
@@ -1044,7 +1046,7 @@ int check_aw(int state[][MAP_LENGTH][MAP_LENGTH], vector start) {
 			return_value = 1;
 		}
 	}
-	
+
 	return return_value;
 }
 
@@ -1064,7 +1066,7 @@ unsigned long long int get_state_priority(int state[][MAP_LENGTH][MAP_LENGTH], i
 	// Priority is always plus value, so we will use unsigned value.
 	unsigned long long int return_value = 0;
 	int d, i, j;
-	
+
 	// Caculate with state value.
 	for (d = 0; d < DIR_MAX; d++) {
 		for (i = 0; i < MAP_LENGTH; i++) {
@@ -1079,9 +1081,9 @@ unsigned long long int get_state_priority(int state[][MAP_LENGTH][MAP_LENGTH], i
 
 int who_win(int map[][MAP_LENGTH], location where_put) {
 	/*	Check map from location 'where_put'
-		if black win	return BLACK;
-		if white win,	return WHITE;
-		else,			return -1;	*/
+	if black win	return BLACK;
+	if white win,	return WHITE;
+	else,			return -1;	*/
 	int i = 0;
 	int result = -1;
 	int mine = map[where_put.x][where_put.y];
@@ -1160,6 +1162,28 @@ int dir_win(int map[][MAP_LENGTH], location start, location dir, int mine) {
 	}
 }
 
+int is_draw(int map[][MAP_LENGTH])	{
+	//return 1 if draw, else return 0
+	int i, j;
+
+	for (i = 0; i < MAP_LENGTH; i++)	{
+		for (j = 0; j < MAP_LENGTH; j++)	{
+			if (map[i][j] != EMPTY || map[i][j] != BLOCKING)	{
+				break;
+			}
+		}
+		if (j != MAP_LENGTH)	{
+			break;
+		}
+	}
+	if (i != MAP_LENGTH)	{
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
 location find_candidate_location(int map[][MAP_LENGTH], int ms[][MAP_LENGTH][MAP_LENGTH], int es[][MAP_LENGTH][MAP_LENGTH], int priority[], int mine) {
 	// Find candidate location and save it to candidate.
 	// if no more candidate is recommended, save (-1, -1).
@@ -1170,10 +1194,10 @@ location find_candidate_location(int map[][MAP_LENGTH], int ms[][MAP_LENGTH][MAP
 	// 어디에 놓는 것이 내 state priority 를 최대화 시키면서, 상대 state priority 를 최소화 시키는지 확인 해야 한다.
 	// Calculate where to put.
 
-	location return_value, to;
-	location candidate[MAP_LENGTH * MAP_LENGTH*2];
+	location to;
+	location candidate[MAP_LENGTH * MAP_LENGTH * 2];
 	location candidate_real[MAP_LENGTH * MAP_LENGTH];
-	
+
 	unsigned long long int enemy_priority[MAP_LENGTH * MAP_LENGTH], mine_priority[MAP_LENGTH * MAP_LENGTH];
 	// case of all area is candidate.
 	// Find all case can do.
@@ -1181,22 +1205,22 @@ location find_candidate_location(int map[][MAP_LENGTH], int ms[][MAP_LENGTH][MAP
 	unsigned long long int now_enemy = get_state_priority(es, priority);
 	unsigned long long int temp_mine, temp_enemy;
 	unsigned long long int highest_mine, highest_enemy;
-	
+
 	int candidate_number = 0;
-	int andidate_number_real = 0;
-	int i, j, temp_my_index, temp_enemy_index;
-	
+	int candidate_number_real = 0;
+	int i, j;
+
 	int temp_map[MAP_LENGTH][MAP_LENGTH];
 	int temp_ms[DIR_MAX][MAP_LENGTH][MAP_LENGTH], temp_es[DIR_MAX][MAP_LENGTH][MAP_LENGTH];
 
 	map_copy(temp_map, map);
 	highest_mine = -1;
 	highest_enemy = -1;
-	
+
 	for (i = 0; i < MAP_LENGTH; i++) {
 		to.x = i;
 		for (j = 0; j < MAP_LENGTH; j++) {
-			to.y = j;	
+			to.y = j;
 			// Check Every area.
 			if (add_stone(to, temp_map, mine) == 1) {
 				// If can do add at some place..
@@ -1205,57 +1229,57 @@ location find_candidate_location(int map[][MAP_LENGTH], int ms[][MAP_LENGTH][MAP
 				temp_enemy = get_state_priority(temp_es, priority);
 				// Caculate changed priority or state.	
 				if (now_mine < temp_mine) {
-					if(highest_mine < temp_mine)	{
+					if (highest_mine < temp_mine)	{
 						highest_mine = temp_mine;
 					}// find highest value of my priority
-					
+
 					location_copy(&to, &candidate[candidate_number]);
 					mine_priority[candidate_number] = temp_mine;
 					enemy_priority[candidate_number] = temp_enemy;
 					// Save it to existing array.
 					candidate_number++;
 				}
-				
+
 				if (now_enemy < temp_enemy) {
-					if(highest_enemy < temp_enemy)	{
+					if (highest_enemy < temp_enemy)	{
 						highest_enemy = temp_enemy;
 					}// find highest value of enemy priority
-					
+
 					location_copy(&to, &candidate[candidate_number]);
 					mine_priority[candidate_number] = temp_mine;
 					enemy_priority[candidate_number] = temp_enemy;
 					// Save it to existing array.
 					candidate_number++;
 				}
-	
+
 				map_copy(temp_map, map);
 				array3_initializer(temp_ms, DIR_MAX, MAP_LENGTH, MAP_LENGTH, NONE_STATE);
 				array3_initializer(temp_es, DIR_MAX, MAP_LENGTH, MAP_LENGTH, NONE_STATE);
 				// Initialize to reuse.
-			}	
+			}
 		}
 	}
-	if( highest_mine - now_mine >= highest_enemy -  now_enemy ) 	{
+	if (highest_mine - now_mine >= highest_enemy - now_enemy) 	{
 		// if attack...
-		for(i = 0; i < candidate_number; i++)	{
-			if(mine_priority[i] == highest_mine)	{
+		for (i = 0; i < candidate_number; i++)	{
+			if (mine_priority[i] == highest_mine)	{
 				// find highest my candidate or candidates
-				location_copy( candidate[i], candidate_real[candidate_number_real] );
+				location_copy(&candidate[i], &candidate_real[candidate_number_real]);
 				candidate_number_real++;
 			}
 		}
 	}
 	else {
 		// if defense...
-		for(i = 0; i < candidate_number; i++)	{
-			if(enemy_priority[i] == highest_enemy)	{
+		for (i = 0; i < candidate_number; i++)	{
+			if (enemy_priority[i] == highest_enemy)	{
 				// find highest enemy's candidate or candidates
-				location_copy( candidate[i], candidate_real[candidate_number_real] );
+				location_copy(&candidate[i], &candidate_real[candidate_number_real]);
 				candidate_number_real++;
 			}
 		}
 	}
-	
+
 	return candidate_real[real_rand(0, candidate_number_real)];
 	// return random value of 0 ~ candidate's number.
 	// this can make sure if highest candidates are many, we can choose randomly location.
@@ -1265,13 +1289,29 @@ int add_stone(location where, int map[][MAP_LENGTH], int mine) {
 	// Add stone to 'where' which stone is 'mine', BLACK, WHITE.
 	// If can't do, return 0, else 1
 
-	if (map[where.x][where.y] == EMPTY) {	
+	if (map[where.x][where.y] == EMPTY) {
 		map[where.x][where.y] = mine;
 		return 1;
 	}
 	else {
 		return 0;
 	}
+}
+
+void ai_turn(int map[][MAP_LENGTH], int priority[], location last_put)	{
+	// real ai part.
+	// AI do his job with given map.
+	// win when can win, defense when can lose.
+	// else, attack or defense with given candidate.
+
+
+}
+
+int ai_game(int map[][MAP_LENGTH], int p0[], int p1[])	{
+	// ai's game with p0, p1 proiority.
+	// return 0 if p0 win, return 1 if p1 win, return -1 if draw.
+
+
 }
 
 void initialize_map(int map[][MAP_LENGTH]) {
@@ -1328,7 +1368,7 @@ void vector_copy(vector *src, vector *dst) {
 	dst->dir = src->dir;
 }
 
-void map_copy(int temp_map[][MAP_LENGTH], int map[][MAP_LENGTH] ) {
+void map_copy(int temp_map[][MAP_LENGTH], int map[][MAP_LENGTH]) {
 	int i, j;
 
 	for (i = 0; i < MAP_LENGTH; i++) {
@@ -1339,21 +1379,26 @@ void map_copy(int temp_map[][MAP_LENGTH], int map[][MAP_LENGTH] ) {
 }
 
 void select_parent(int parent[][STATE_LENGTH], int selected[][STATE_LENGTH], int fitness[]) {
-	// Select 4 parents from parent[GENERATION_MAX][STATE_LENGTH] with fitness[GENERATION_MAX] values.
+	// Select PARAENT_MAX parents from parent[GENERATION_MAX][STATE_LENGTH] with fitness[GENERATION_MAX] values.
 	// We will use rullet-wheel method to select parents.
 	int i, j, temp;
 	int index[GENERATION_MAX];
+	int temp_fit[GENERATION_MAX];
 	for (i = 0; i < GENERATION_MAX; i++) {
 		index[i] = i;
 	}
 
-	// Sort with fitness value and take top 4.
+	for (i = 0; i < GENERATION_MAX; i++)	{
+		temp_fit[i] = fitness[i];
+	}
+
+	// Sort with fitness value and take top PARAENT_MAX.
 	for (i = 0; i < GENERATION_MAX - 1; i++) {
 		for (j = i + 1; j < GENERATION_MAX; j++) {
-			if (fitness[i] < fitness[j]) {
-				temp = fitness[i];
-				fitness[i] = fitness[j];
-				fitness[j] = temp;
+			if (temp_fit[i] < temp_fit[j]) {
+				temp = temp_fit[i];
+				temp_fit[i] = temp_fit[j];
+				temp_fit[j] = temp;
 
 				temp = index[i];
 				index[i] = index[j];
@@ -1362,18 +1407,24 @@ void select_parent(int parent[][STATE_LENGTH], int selected[][STATE_LENGTH], int
 			}
 		}
 	}
+
 	for (i = 0; i < PARENT_MAX; i++) {
+		// Select top PARAENT_MAX values.
 		for (j = 0; j < STATE_LENGTH; j++) {
 			selected[i][j] = parent[index[i]][j];
 		}
 	}
 }
 
-void set_fitness(int generation[][STATE_LENGTH], int fitness[]) {
+void set_fitness(int generation[][STATE_LENGTH], int given_set[], int fitness[]) {
 	// Set fitness score to fitenss[GENERATION_MAX].
-	// fitness will be win percent to others.
-	// More specific, fitness[i] is probability of victory for others.
+	// generation[i] VS given_set 's win percent will be fitness[i]
 	// Probability of victory is average of result of every other generations.
+
+	int map[MAP_LENGTH][MAP_LENGTH];
+	int win;
+	initialize_map(map);
+	win = ai_game();
 
 }
 
@@ -1425,8 +1476,7 @@ void generate_cross_point(int cross[]) {
 	int i, j, temp;
 	// Generate random cross-point.
 	for (i = 0; i < CROSS_POINT_MAX; i++) {
-		// rand() need to chagned.
-		cross[i] = rand() % STATE_LENGTH;
+		cross[i] = real_rand(0, STATE_LENGTH);
 	}
 
 	// Sorting with large number.
@@ -1441,7 +1491,7 @@ void generate_cross_point(int cross[]) {
 	}
 }
 
-void cross_product(int p0[], int p1[], int gen[] ,int cross[]) {
+void cross_product(int p0[], int p1[], int gen[], int cross[]) {
 	// Doing cross product.
 	// gen = { p1|p2|p1|p2 ... } for cross[i] in range(0, CORSS_POINT_MAX)
 	int par = 0;
@@ -1478,14 +1528,10 @@ void generate_mutation(int generation[][STATE_LENGTH], int fitness[]) {
 	// Mutation is appear at generation[GENERATION_MAX][STATE_LENGTH] with percent of fitness[].
 	// Mutation appear at [i] change into random number.
 	// The probability of mutation will be low if parent's fitness average is low.
-	
+	// fitess[i] is over 0, below FITNESS_MAX, so if real_rand(0, FITNESS_MAX) 's output is over fitness[i], then mutate.
+	// if else, don't mutate.
+	// for all part of generic.
 
-
-}
-
-void suffle_generation(int generation[][STATE_LENGTH]) {
-	// generation is generated with in order of parents.
-	// so suffle is needed.
 
 }
 
@@ -1495,7 +1541,7 @@ int real_rand(int from, int to)	{
 	srand((unsigned)time(NULL));
 	// Seed Initialize.
 
-	while(return_value < from && return_value >= to) {
+	while (return_value < from && return_value >= to) {
 		// If out of range, iterate.
 		return_value = rand();
 	}
